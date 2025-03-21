c0bc9e43b4125ef97e8477af56d1276480f8f6d2
Steven <sc201789@ucf.edu>
2025-03-21 15:40:33 -0400

    backend server base definition

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,52 @@
 const express = require('express');
 const app = express();
-const port = 5001;
+const cors = require('cors');
+require('./Schemas/association');
 
-// Middleware to handle JSON requests
+const sequelize = require('./database/database');
+
+//connect to database
+sequelize.authenticate()
+    .then(() => console.log("database connected"))
+    .catch((err) => console.error("error in connecting to database:", err));
+
+//ensure tables exist
+async function InitializeDatabase(){
+  try{
+    await sequelize.sync ({force:false});
+    console.log("database tables there already or now created");
+  }catch(error){
+    console.error('failed tables', error);
+  }
+}
+InitializeDatabase();
+
+//which origin is allowed to access my backend
+const corsOptions = {
+  origin: 'http://localhost:3001',
+  methods: 'GET,POST'
+}
+
+//apply cors middleware to every route in my app
+app.use(cors(corsOptions));
+
+//allow json parsing
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Test route
-app.get('/', (req, res) => {
-  res.send('Backend is running!');
-});
+//user routes
+const userRouter = require('./Routes/users');
+app.use('/user', userRouter);
+
+//job routes
+const jobRouter = require('./Routes/jobs');
+app.use('/job', jobRouter);
+
+//define which port to use
+const port = 5001;
 
 // Start the server
 app.listen(port, () => {
   console.log(`Backend is running at http://localhost:${port}`);
 });
+
+module.exports = app;
